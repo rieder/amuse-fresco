2b0d48a9f54e3e66a473db2308fc35b2f2904963
Steven Rieder <steven@rieder.nl>
2017-07-10 15:54:32 +0900

    Py3 type prints

--- fresco.py
+++ fresco.py
@@ -1,46 +1,56 @@
-# -*- coding: utf-8 -*- 
-import os,sys
+# -*- coding: utf-8 -*-
+from __future__ import (
+        print_function,
+        )
+import sys
 
-import numpy as np
+# import numpy as np
 
-from amuse.units import units,constants,nbody_system
+from amuse.units import units, constants
 from amuse.io import read_set_from_file
-#from amuse.ext.job_server import JobServer
-#from PIL import Image
-#from ubvivariant import rgb_frame
 from ubvinew import rgb_frame
 
-import matplotlib
+# import matplotlib
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+
 def calculate_effective_temperature(luminosity, radius):
-    return ( (luminosity/(constants.four_pi_stefan_boltzmann*radius**2)
-        )**.25).in_(units.K)
+    return (
+            (
+                luminosity / (constants.four_pi_stefan_boltzmann*radius**2)
+                )**.25
+            ).in_(units.K)
+
 
 def image_from_stars(
         stars,
-        image_width         = 10. | units.parsec,
-        image_size          = [1024,1024],
-        percentile          = 0.9995,
-        calc_temperature    = True,
-        age                 = 0.|units.Myr,
+        image_width=10. | units.parsec,
+        image_size=[1024, 1024],
+        percentile=0.9995,
+        calc_temperature=True,
+        age=0. | units.Myr,
         ):
     if calc_temperature:
+        # calculates the temperature of the stars from their total luminosity
+        # and radius, calculates those first if needed
         try:
             stars.temperature = calculate_effective_temperature(
                     stars.luminosity,
                     stars.radius,
                     )
         except:
-            print "Calculating luminosity/temperature for %s old stars..."%(
-                    age )
+            print(
+                    "Calculating luminosity/temperature for %s old stars..."
+                    % (age)
+                    )
             from amuse.community.sse.interface import SSE
             se = SSE()
             se.particles.add_particles(stars)
-            if age > 0|units.Myr:
-                print "Evolve start"
+            if age > 0 | units.Myr:
+                print("Evolve start")
                 se.evolve_model(age)
-                print "Evolve done"
+                print("Evolve done")
             stars.luminosity = se.particles.luminosity
             stars.radius = se.particles.radius
             stars.temperature = calculate_effective_temperature(
@@ -49,47 +59,49 @@ def image_from_stars(
                     )
             se.stop()
 
-    print "Start making RGB image"
+    print("Start making RGB image")
     vmax, rgb = rgb_frame(
             stars,
-            dryrun      = False,
-            image_width = image_width,
-            multi_psf   = False,#True,
-            image_size  = image_size,
-            percentile  = percentile,
-            sourcebands = "ubvri",
+            dryrun=False,
+            image_width=image_width,
+            multi_psf=False,  # True,
+            image_size=image_size,
+            percentile=percentile,
+            sourcebands="ubvri",
             )
     return rgb['pixels']
 
 
-if __name__=="__main__":
-    filename    = sys.argv[1]
-    
+if __name__ == "__main__":
+    filename = sys.argv[1]
+
     length_unit = units.parsec
-    dpi         = 600
+    dpi = 600
     image_width_arcsec = 160
 
-    image_width = 10. | units.parsec #FIXME this should depend on the distance!
-                                     # size = fixed at nr of arcmin
-                                     
-    image_size  = [2048,2048] #FIXME the psf is fixed pixel size, so the pixels
-    # in the image here reflects how much pixels will be spread!
-    # In principle, this should be a fixed number, equal to the number of
-    # pixels of the ccd for which the psf was made.
-    # Also, the image should reflect the distance to the "observed" cluster.
+    # FIXME this should depend on the distance!
+    # size = fixed at nr of arcmin
+    image_width = 10. | units.parsec
 
-    age         = 500.|units.Myr
-    percentile  = 0.9995 ## for determining vmax
+    # FIXME the psf is fixed pixel size, so the pixels in the image here
+    # reflects how much pixels will be spread!  In principle, this should be a
+    # fixed number, equal to the number of pixels of the ccd for which the psf
+    # was made.  Also, the image should reflect the distance to the "observed"
+    # cluster.
+    image_size = [2048, 2048]
 
-    xmin        = -0.5 * image_width.value_in(length_unit)
-    xmax        =  0.5 * image_width.value_in(length_unit)
-    ymin        = -0.5 * image_width.value_in(length_unit)
-    ymax        =  0.5 * image_width.value_in(length_unit)
+    age = 500. | units.Myr
+    percentile = 0.9995  # for determining vmax
 
-    stars       = read_set_from_file(
+    xmin = -0.5 * image_width.value_in(length_unit)
+    xmax = 0.5 * image_width.value_in(length_unit)
+    ymin = -0.5 * image_width.value_in(length_unit)
+    ymax = 0.5 * image_width.value_in(length_unit)
+
+    stars = read_set_from_file(
             filename,
             "amuse",
-            close_file  = True,
+            close_file=True,
             )
 
     # FIXME: add these features
@@ -100,33 +112,33 @@ if __name__=="__main__":
     imagefilename = sys.argv[1]+".png"
 
     fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.set_xlim([xmin,xmax])
-    ax.set_ylim([ymin,ymax])
-    ax.set_xlabel("[%s]"%(length_unit))
-    ax.set_ylabel("[%s]"%(length_unit))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlim([xmin, xmax])
+    ax.set_ylim([ymin, ymax])
+    ax.set_xlabel("[%s]" % (length_unit))
+    ax.set_ylabel("[%s]" % (length_unit))
 
     image = image_from_stars(
             stars,
-            image_width         = image_width,
-            image_size          = image_size,
-            percentile          = percentile,
-            calc_temperature    = True,
-            age                 = age,
+            image_width=image_width,
+            image_size=image_size,
+            percentile=percentile,
+            calc_temperature=True,
+            age=age,
             )
 
     plt.imshow(
             image,
-            origin  = 'lower',
-            extent  = [
+            origin='lower',
+            extent=[
                 xmin,
                 xmax,
                 ymin,
                 ymax,
                 ],
             )
-    
+
     plt.savefig(
             imagefilename,
-            dpi = dpi,
+            dpi=dpi,
             )
