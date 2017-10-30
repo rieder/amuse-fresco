5f060b1448f911f382d45633bbcc226a15ef7c66
Steven Rieder <steven@rieder.nl>
2017-10-30 15:29:06 +0900

    Interpolate x and y between pixels

--- fresco/gridify.py
+++ fresco/gridify.py
@@ -59,21 +59,37 @@ def map_to_2d_grid_simple(
     except:
         x_width = image_width
         y_width = image_width
-    length_unit = x_width.unit
 
-    xbins = (
-            np.linspace(-0.5, 0.5, num=1+x_size)
-            * x_width.value_in(length_unit)
-            )
-    ybins = (
-            np.linspace(-0.5, 0.5, num=1+y_size)
-            * y_width.value_in(length_unit)
-            )
-    result = np.histogram2d(
-            x.value_in(length_unit),
-            y.value_in(length_unit),
-            bins=[xbins, ybins],
-            weights=weights,
-            )
+    # Calculate the X and Y position in pixels
+    # width is centred on 0, so shift by 0.5 image size!
+    x_px = x_size * ((x / x_width) + 0.5)
+    y_px = y_size * ((y / y_width) + 0.5)
 
-    return result[0]
+    x_0 = np.floor(x_px)
+    y_0 = np.floor(y_px)
+    x_1 = x_0 + 1
+    y_1 = y_0 + 1
+    weight_x0 = (x_1 - x_px)
+    weight_y0 = (y_1 - y_px)
+    weight_x1 = (x_px - x_0)
+    weight_y1 = (y_px - y_0)
+    pos_weights = [0, 0, 0, 0]
+    pos_weights[0] = weight_x0 * weight_y0
+    pos_weights[1] = weight_x0 * weight_y1
+    pos_weights[2] = weight_x1 * weight_y0
+    pos_weights[3] = weight_x1 * weight_y1
+
+    xbins = np.arange(1+x_size)
+    ybins = np.arange(1+y_size)
+
+    result = np.zeros(x_size*y_size).reshape(x_size, y_size)
+    for i in range(2):
+        for j in range(2):
+            result += np.histogram2d(
+                    x_0 + i,
+                    y_0 + j,
+                    bins=[xbins, ybins],
+                    weights=weights * pos_weights[2*i+j],
+                    )[0]
+
+    return result
