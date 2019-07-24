0eef76bdf55431aa5aca2178d32d85200000cb14
Steven Rieder <steven@rieder.nl>
2019-07-24 16:42:29 +0100

    bugfix and split into module / standalone

--- test/test.sh
+++ test/test.sh
@@ -3,3 +3,4 @@ python test_set.py
 python ../fresco.py -s stars.hdf5 -o test_stars
 python ../fresco.py -g gas.hdf5 -o test_gas
 python ../fresco.py -s stars.hdf5 -g gas.hdf5 -o test_starsgas
+python ../fresco.py -s stars.hdf5 -g gas.hdf5 -o test_starsgasext --ext
