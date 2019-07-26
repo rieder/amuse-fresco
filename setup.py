42484f2084451689565fadb4bdc22ae68610113c
Steven Rieder <steven@rieder.nl>
2019-07-26 11:53:08 +0100

    Add fresco.py script

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = "0.5.0"
+version = "0.5.1"
 name = 'amuse-fresco'
 author = 'Steven Rieder and Inti Pelupessy'
 author_email = 'steven@rieder.nl'
@@ -70,4 +70,5 @@ setup(
     data_files=all_data_files,
     include_package_data=True,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    scripts=["fresco.py"],
 )
