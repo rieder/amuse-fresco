d10ac848c4b10cb6024efd8ae22cb3398af2de5e
Steven Rieder <steven@rieder.nl>
2024-04-30 15:56:44 -0400

    refactoring

--- src/amuse/plot/fresco/fieldstars.py
+++ src/amuse/plot/fresco/fieldstars.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import (
-        print_function,
-        division,
-        )
 import numpy as np
 from amuse.datamodel import Particles
 from amuse.units import units
@@ -10,20 +5,20 @@ from amuse.ic.salpeter import new_salpeter_mass_distribution
 
 
 def new_field_stars(
-        N,
-        width=10 | units.parsec,
-        height=10 | units.parsec,
-        depth=100 | units.parsec,
-        massdistribution="salpeter",
-        agespread=3 | units.Gyr,
-        seed=1701,
-        ):
+    number_of_stars,
+    width=10 | units.parsec,
+    height=10 | units.parsec,
+    depth=100 | units.parsec,
+    massdistribution="salpeter",
+    agespread=3 | units.Gyr,
+    seed=1701,
+):
     np.random.seed(seed)
-    stars = Particles(N)
-    stars.x = (np.random.random(N)-0.5) * width
-    stars.y = (np.random.random(N)-0.5) * height
-    stars.z = (np.random.random(N)-0.02) * depth
+    stars = Particles(number_of_stars)
+    stars.x = (np.random.random(number_of_stars) - 0.5) * width
+    stars.y = (np.random.random(number_of_stars) - 0.5) * height
+    stars.z = (np.random.random(number_of_stars) - 0.02) * depth
     if massdistribution == "salpeter":
-        stars.mass = new_salpeter_mass_distribution(N)
+        stars.mass = new_salpeter_mass_distribution(number_of_stars)
 
     return stars
