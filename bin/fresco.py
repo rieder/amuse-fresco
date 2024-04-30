786862a0fb0adf561a4e408078e60a115d2582f9
Steven Rieder <steven@rieder.nl>
2024-04-30 15:49:54 -0400

    reformat bin/fresco.py

--- bin/fresco.py
+++ bin/fresco.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
 Fresco creates a "simulated observation" of a set of particles.
 Particles can be "stars" (point sources emitting light) or "gas" (emitting,
@@ -14,7 +12,8 @@ import argparse
 import numpy as np
 
 import matplotlib
-matplotlib.use('Agg')
+
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 from amuse.units import units, nbody_system
@@ -38,188 +37,186 @@ def fresco_argument_parser(parser=None):
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         )
     parser.add_argument(
-        '--filetype',
-        dest='filetype',
-        default='amuse',
-        help='file type, valid are all types AMUSE can read',
+        "--filetype",
+        dest="filetype",
+        default="amuse",
+        help="file type, valid are all types AMUSE can read",
     )
     parser.add_argument(
-        '-s',
-        dest='starsfilename',
-        help='file containing stars (optional)',
+        "-s",
+        dest="starsfilename",
+        help="file containing stars (optional)",
     )
     parser.add_argument(
-        '-g',
-        dest='gasfilename',
-        help='file containing gas (optional)',
+        "-g",
+        dest="gasfilename",
+        help="file containing gas (optional)",
     )
     parser.add_argument(
-        '-f',
-        dest='followfilename',
+        "-f",
+        dest="followfilename",
+        help=("file containing star keys to center on (optional, implies --com)"),
+    )
+    parser.add_argument(
+        "-o",
+        dest="imagefilename",
+        help="write image to this file",
+    )
+    parser.add_argument(
+        "--imagetype",
+        dest="imagetype",
+        default="png",
+        help="image file type",
+    )
+    parser.add_argument(
+        "-b",
+        dest="sourcebands",
+        default="ubvri",
+        help="colour bands to use",
+    )
+    parser.add_argument(
+        "-a",
+        dest="age",
+        default=100 | units.Myr,
+        type=units.Myr,
+        help="age of the stars",
+    )
+    parser.add_argument(
+        "-w",
+        dest="width",
+        default=5 | units.pc,
+        type=units.pc,
+        help="image width",
+    )
+    parser.add_argument(
+        "-x",
+        dest="plot_axes",
+        action="store_true",
+        default=False,
+        help="plot axes",
+    )
+    parser.add_argument(
+        "--ext",
+        dest="calculate_extinction",
+        action="store_true",
+        default=False,
+        help="include extinction by dust",
+    )
+    parser.add_argument(
+        "--seed",
+        dest="seed",
+        default=1701,
+        type=int,
+        help="random seed",
+    )
+    parser.add_argument(
+        "--vmax",
+        dest="vmax",
+        default=0,
+        type=float,
+        help="vmax value",
+    )
+    parser.add_argument(
+        "--field",
+        dest="n_fieldstars",
+        default=0,
+        type=int,
+        help="add N field stars (optional)",
+    )
+    parser.add_argument(
+        "--ax",
+        dest="angle_x",
+        default=0 | units.deg,
+        type=units.deg,
+        help="Rotation step around x-axis",
+    )
+    parser.add_argument(
+        "--ay",
+        dest="angle_y",
+        default=0 | units.deg,
+        type=units.deg,
+        help="Rotation step around y-axis",
+    )
+    parser.add_argument(
+        "--az",
+        dest="angle_z",
+        default=0 | units.deg,
+        type=units.deg,
+        help="Rotation step around z-axis",
+    )
+    parser.add_argument(
+        "--frames",
+        dest="frames",
+        default=1,
+        type=int,
+        help="Number of frames (>1: rotate around x,y,z)",
+    )
+    parser.add_argument(
+        "--px",
+        dest="pixels",
+        default=2048,
+        type=int,
+        help="Number of pixels along each axis",
+    )
+    parser.add_argument(
+        "--psf",
+        dest="psf_type",
+        default="hubble",
         help=(
-            'file containing star keys to center on (optional, implies --com)'
+            "PSF type. Looks for a .fits file of the given name, uses this if "
+            "it exists.\n"
+            'Otherwise, "hubble", "wfc3", "wfpc2" and "gaussian" are valid '
+            "options."
         ),
     )
     parser.add_argument(
-        '-o',
-        dest='imagefilename',
-        help='write image to this file',
-    )
-    parser.add_argument(
-        '--imagetype',
-        dest='imagetype',
-        default='png',
-        help='image file type',
-    )
-    parser.add_argument(
-        '-b',
-        dest='sourcebands',
-        default='ubvri',
-        help='colour bands to use',
-    )
-    parser.add_argument(
-        '-a',
-        dest='age',
-        default=100 | units.Myr,
-        type=units.Myr,
-        help='age of the stars',
-    )
-    parser.add_argument(
-        '-w',
-        dest='width',
-        default=5 | units.pc,
-        type=units.pc,
-        help='image width',
-    )
-    parser.add_argument(
-        '-x',
-        dest='plot_axes',
-        action='store_true',
-        default=False,
-        help='plot axes',
-    )
-    parser.add_argument(
-        '--ext',
-        dest='calculate_extinction',
-        action='store_true',
-        default=False,
-        help='include extinction by dust',
-    )
-    parser.add_argument(
-        '--seed',
-        dest='seed',
-        default=1701,
-        type=int,
-        help='random seed',
-    )
-    parser.add_argument(
-        '--vmax',
-        dest='vmax',
-        default=0,
-        type=float,
-        help='vmax value',
-    )
-    parser.add_argument(
-        '--field',
-        dest='n_fieldstars',
-        default=0,
-        type=int,
-        help='add N field stars (optional)',
-    )
-    parser.add_argument(
-        '--ax',
-        dest='angle_x',
-        default=0 | units.deg,
-        type=units.deg,
-        help='Rotation step around x-axis',
-    )
-    parser.add_argument(
-        '--ay',
-        dest='angle_y',
-        default=0 | units.deg,
-        type=units.deg,
-        help='Rotation step around y-axis',
-    )
-    parser.add_argument(
-        '--az',
-        dest='angle_z',
-        default=0 | units.deg,
-        type=units.deg,
-        help='Rotation step around z-axis',
-    )
-    parser.add_argument(
-        '--frames',
-        dest='frames',
-        default=1,
-        type=int,
-        help='Number of frames (>1: rotate around x,y,z)',
-    )
-    parser.add_argument(
-        '--px',
-        dest='pixels',
-        default=2048,
-        type=int,
-        help='Number of pixels along each axis',
-    )
-    parser.add_argument(
-        '--psf',
-        dest='psf_type',
-        default='hubble',
-        help=(
-            'PSF type. Looks for a .fits file of the given name, uses this if '
-            'it exists.\n'
-            'Otherwise, "hubble", "wfc3", "wfpc2" and "gaussian" are valid '
-            'options.'
-        )
-    )
-    parser.add_argument(
-        '--sigma',
-        dest='psf_sigma',
+        "--sigma",
+        dest="psf_sigma",
         default=1.0,
         type=float,
-        help='PSF sigma (if PSF type is gaussian)',
+        help="PSF sigma (if PSF type is gaussian)",
     )
     parser.add_argument(
-        '--fl',
-        dest='fixed_luminosity',
-        action='store_true',
+        "--fl",
+        dest="fixed_luminosity",
+        action="store_true",
         default=False,
-        help='Use a fixed, equal luminosity and temperature for all stars',
+        help="Use a fixed, equal luminosity and temperature for all stars",
     )
     parser.add_argument(
-        '--contours',
-        dest='contours',
-        action='store_true',
+        "--contours",
+        dest="contours",
+        action="store_true",
         default=False,
-        help='Plot gas contour lines',
+        help="Plot gas contour lines",
     )
     parser.add_argument(
-        '--com',
-        dest='use_com',
-        action='store_true',
+        "--com",
+        dest="use_com",
+        action="store_true",
         default=False,
-        help='Center on center of mass',
+        help="Center on center of mass",
     )
     parser.add_argument(
-        '--xo',
-        dest='x_offset',
+        "--xo",
+        dest="x_offset",
         default=0.0 | units.pc,
         type=units.pc,
-        help='X offset',
+        help="X offset",
     )
     parser.add_argument(
-        '--yo',
-        dest='y_offset',
+        "--yo",
+        dest="y_offset",
         default=0.0 | units.pc,
         type=units.pc,
-        help='Y offset',
+        help="Y offset",
     )
     parser.add_argument(
-        '--zo',
-        dest='z_offset',
+        "--zo",
+        dest="z_offset",
         default=0.0 | units.pc,
         type=units.pc,
-        help='Z offset',
+        help="Z offset",
     )
     return parser.parse_args()
 
@@ -279,7 +276,7 @@ def main():
 
     image_size = [pixels, pixels]
     # If the nr of pixels is changed, zoom the PSF accordingly.
-    zoom_factor = pixels / 2048.
+    zoom_factor = pixels / 2048.0
 
     if starsfilename:
         stars = read_set_from_file(
@@ -288,10 +285,7 @@ def main():
             close_file=True,
         )
         if stellar_evolution and (age > 0 | units.Myr):
-            print((
-                "Calculating luminosity/temperature for %s old stars..."
-                % (age)
-            ))
+            print(("Calculating luminosity/temperature for %s old stars..." % (age)))
             evolve_to_age(stars, age, stellar_evolution=se_code)
         elif args.fixed_luminosity:
             for band in sourcebands:
@@ -299,7 +293,9 @@ def main():
         if use_com:
             if followfilename is not None:
                 followstars = read_set_from_file(
-                    followfilename, filetype, close_file=True,
+                    followfilename,
+                    filetype,
+                    close_file=True,
                 )
                 center_on_these_stars = followstars.get_intersecting_subset_in(
                     stars,
@@ -317,16 +313,16 @@ def main():
             maxx = stars.x.max()
             miny = stars.y.min()
             maxy = stars.y.max()
-            image_width = max(maxx-minx, maxy-miny)
-            x_offset = (maxx+minx) / 2
-            y_offset = (maxy+miny) / 2
+            image_width = max(maxx - minx, maxy - miny)
+            x_offset = (maxx + minx) / 2
+            y_offset = (maxy + miny) / 2
         stars.x -= x_offset
         stars.y -= y_offset
         stars.z -= z_offset
-        minx = (1.5 * -image_width/2)
-        maxx = (1.5 * image_width/2)
-        miny = (1.5 * -image_width/2)
-        maxy = (1.5 * image_width/2)
+        minx = 1.5 * -image_width / 2
+        maxx = 1.5 * image_width / 2
+        miny = 1.5 * -image_width / 2
+        maxy = 1.5 * image_width / 2
         stars = stars[stars.x > minx]
         stars = stars[stars.x < maxx]
         stars = stars[stars.y > miny]
@@ -342,13 +338,7 @@ def main():
             width=image_width,
             height=image_width,
         )
-        fieldstars.age = (
-            minage
-            + (
-                np.random.sample(n_fieldstars)
-                * (maxage - minage)
-            )
-        )
+        fieldstars.age = minage + (np.random.sample(n_fieldstars) * (maxage - minage))
         evolve_to_age(fieldstars, 0 | units.yr, stellar_evolution=se_code)
         stars.add_particles(fieldstars)
 
@@ -371,17 +361,12 @@ def main():
         # gas.radius = gas.h_smooth
 
         # Select only the relevant gas particles (plus a margin)
-        minx = (1.1 * -image_width/2)
-        maxx = (1.1 * image_width/2)
-        miny = (1.1 * -image_width/2)
-        maxy = (1.1 * image_width/2)
+        minx = 1.1 * -image_width / 2
+        maxx = 1.1 * image_width / 2
+        miny = 1.1 * -image_width / 2
+        maxy = 1.1 * image_width / 2
         gas_ = gas.select(
-            lambda x, y:
-            x > minx
-            and x < maxx
-            and y > miny
-            and y < maxy,
-            ["x", "y"]
+            lambda x, y: x > minx and x < maxx and y > miny and y < maxy, ["x", "y"]
         )
         gas = gas_
     else:
@@ -409,9 +394,16 @@ def main():
     ymin, ymax = ax.get_ylim()
 
     if not stars.is_empty():
-        rotate(stars, (frames-1)*angle_x, (frames-1)*angle_y, (frames-1)*angle_z)
+        rotate(
+            stars,
+            (frames - 1) * angle_x,
+            (frames - 1) * angle_y,
+            (frames - 1) * angle_z,
+        )
     if not gas.is_empty():
-        rotate(gas, (frames-1)*angle_x, (frames-1)*angle_y, (frames-1)*angle_z)
+        rotate(
+            gas, (frames - 1) * angle_x, (frames - 1) * angle_y, (frames - 1) * angle_z
+        )
     for frame in range(frames):
         frame += frames
         print(
@@ -451,7 +443,7 @@ def main():
         if not stars.is_empty():
             ax.imshow(
                 image,
-                origin='lower',
+                origin="lower",
                 extent=[
                     xmin,
                     xmax,
@@ -470,17 +462,20 @@ def main():
                 vmax = np.max(gascontours) / 2
                 # vmin = np.min(image[np.where(image > 0.0)])
                 vmin = vmax / 100
-                levels = 10**(
-                    np.linspace(
-                        np.log10(vmin),
-                        np.log10(vmax),
-                        num=5,
-                    )
-                )[1:]
+                levels = (
+                    10
+                    ** (
+                        np.linspace(
+                            np.log10(vmin),
+                            np.log10(vmax),
+                            num=5,
+                        )
+                    )[1:]
+                )
                 # print(vmin, vmax)
                 # print(levels)
                 ax.contour(
-                    origin='lower',
+                    origin="lower",
                     levels=levels,
                     colors="white",
                     linewidths=0.1,
@@ -500,7 +495,7 @@ def main():
 
             ax.imshow(
                 image,
-                origin='lower',
+                origin="lower",
                 extent=[
                     xmin,
                     xmax,
